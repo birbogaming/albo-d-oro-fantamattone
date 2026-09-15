079340699790f97fe71a12920d179d53a8daeac3
birbogaming <birbogaming@protonmail.com>
2026-09-15 10:10:00 +0200

    Aggiunta cartella wwwroot

---
+++ wwwroot/service-worker-assets.js
@@ -0,0 +1,453 @@
+self.assetsManifest = {
+  "version": "lW9ADLGY",
+  "assets": [
+    {
+      "hash": "sha256-Nqnn8clbgv+5l0PgxcTOldg8mkMKrFn4TvPL+rYUUGg=",
+      "url": ".nojekyll"
+    },
+    {
+      "hash": "sha256-lN14Tro/67cTY82fb2Os5IvZFJL+J17ZsT4G4UYBs3g=",
+      "url": "404.html"
+    },
+    {
+      "hash": "sha256-4bn2CrKRmym7QJHuVhQCXNwWavo1101TmKZ/ngdsZ6c=",
+      "url": "_framework/AlboFantamattoneBlazor.wasm"
+    },
+    {
+      "hash": "sha256-jNb71XRCaXF6JJPXEQkuUY1JFKY+Tz/LTm4PP3LsUvg=",
+      "url": "_framework/Microsoft.AspNetCore.Components.Web.wasm"
+    },
+    {
+      "hash": "sha256-HkY/ueMntnr2loHI/qQbYwrBnVrVrWLVE9P3W1+4GHo=",
+      "url": "_framework/Microsoft.AspNetCore.Components.WebAssembly.wasm"
+    },
+    {
+      "hash": "sha256-rKvSvM9PKNZ3aXZjhtLPi1zNz4fnjZri1ceVpOaNvbc=",
+      "url": "_framework/Microsoft.AspNetCore.Components.wasm"
+    },
+    {
+      "hash": "sha256-XR+WUfg8LtnrwdXFBnV+qO3ROyp6cYCfl1AZajIAheM=",
+      "url": "_framework/Microsoft.Extensions.Configuration.Abstractions.wasm"
+    },
+    {
+      "hash": "sha256-3UBRwvCkKtraRUarsIBDS05tA2rRm1leCB6EDfyVjeA=",
+      "url": "_framework/Microsoft.Extensions.Configuration.Json.wasm"
+    },
+    {
+      "hash": "sha256-od574rRtVVRJj+24jbl8j3GdQFpxlJOpSx+pEMw1Q18=",
+      "url": "_framework/Microsoft.Extensions.Configuration.wasm"
+    },
+    {
+      "hash": "sha256-9bD5w24+ziVSGPkkYPjRaSZI9dMm9QLGYsqCRVARNtM=",
+      "url": "_framework/Microsoft.Extensions.DependencyInjection.Abstractions.wasm"
+    },
+    {
+      "hash": "sha256-mw/Sy1n9J6R0bUD0GI5MtB/zP+wyzOPiVsOTA/1C4NA=",
+      "url": "_framework/Microsoft.Extensions.DependencyInjection.wasm"
+    },
+    {
+      "hash": "sha256-HbtMUzIIXgi7lfYQDxb6l6aMWu5jkNv6U/xZl/4uggo=",
+      "url": "_framework/Microsoft.Extensions.Logging.Abstractions.wasm"
+    },
+    {
+      "hash": "sha256-lronxO5RVywLbFhme/ExIuzFePlP8EosZyZteUaMAwE=",
+      "url": "_framework/Microsoft.Extensions.Logging.wasm"
+    },
+    {
+      "hash": "sha256-mngxZ7DtB5tYzmxZdVWZapJTSJ/yGClqQAcqOkLk3Lk=",
+      "url": "_framework/Microsoft.Extensions.Options.wasm"
+    },
+    {
+      "hash": "sha256-Bi1hyBMWneH/ByWbqT/pNe0B7I/NQFklzD2g3Whdqlk=",
+      "url": "_framework/Microsoft.Extensions.Primitives.wasm"
+    },
+    {
+      "hash": "sha256-DwdIH1ydDREMe+gT+NPaWxTUdDh/bzbriY2XoySGIo4=",
+      "url": "_framework/Microsoft.JSInterop.WebAssembly.wasm"
+    },
+    {
+      "hash": "sha256-x95sJKmucIA620K9WKevOHlcJVchktBN6fWmMhLQ6eQ=",
+      "url": "_framework/Microsoft.JSInterop.wasm"
+    },
+    {
+      "hash": "sha256-qbsoldWAEqbxBcN9k5FR1gtOjU+mQ+MDmuZdvSyCqXQ=",
+      "url": "_framework/System.Collections.Concurrent.wasm"
+    },
+    {
+      "hash": "sha256-Bwd5jyfaELUwowrXG1az4NCgq37gkiGsQLDYyVhaM98=",
+      "url": "_framework/System.Collections.wasm"
+    },
+    {
+      "hash": "sha256-PsFt2WhF4EluopSKxqrMexySPGfDxERH8Ejn7UJa8T8=",
+      "url": "_framework/System.ComponentModel.wasm"
+    },
+    {
+      "hash": "sha256-AFXXMMB5glzVXUwRZLwBej+k81HAsCwkFskYmFI3gQs=",
+      "url": "_framework/System.Console.wasm"
+    },
+    {
+      "hash": "sha256-xDtdm9hM6DtsL6EngW3KL7MyUEdf5vi0KBjDdQexaMg=",
+      "url": "_framework/System.Diagnostics.DiagnosticSource.wasm"
+    },
+    {
+      "hash": "sha256-X+TgRgK1zJMEjs1r2OGMneq9eaPpjOld0fdGojwC8Wo=",
+      "url": "_framework/System.Linq.wasm"
+    },
+    {
+      "hash": "sha256-TsB+1bCNsXpQNbd7NsxTA9qufJkwg5PG8rOnDZCHXlA=",
+      "url": "_framework/System.Memory.wasm"
+    },
+    {
+      "hash": "sha256-ZjDmSM7toRIAqslWw4I1U91Jor4by0mMo2J3EuRvdho=",
+      "url": "_framework/System.Net.Http.Json.wasm"
+    },
+    {
+      "hash": "sha256-HJeSlCUPMzDXogKw08eVTAT2g8zRi3C50BKfrYlHEks=",
+      "url": "_framework/System.Net.Http.wasm"
+    },
+    {
+      "hash": "sha256-kwUKMCcHFdIZfQRqqxqVgbc0u9torgO9Cy4lg+wYWCE=",
+      "url": "_framework/System.Net.Primitives.wasm"
+    },
+    {
+      "hash": "sha256-A5STD3uvAWgaQR/tCY+sm8TOOc2BJnGo6Zgehj7zcRs=",
+      "url": "_framework/System.Private.CoreLib.wasm"
+    },
+    {
+      "hash": "sha256-dlp9SWb172ZoDMHHG6gVbBjl41hl1XXxGyfjtEo+dGA=",
+      "url": "_framework/System.Private.Uri.wasm"
+    },
+    {
+      "hash": "sha256-oRCN88yd3lBXRfAmWROwi3OJecv7kjkEDCqOInSy6lY=",
+      "url": "_framework/System.Runtime.InteropServices.JavaScript.wasm"
+    },
+    {
+      "hash": "sha256-+mFXN7OwFO5FiJ8Ev54GaPeqsIORMhjotTYweFnT8qc=",
+      "url": "_framework/System.Runtime.wasm"
+    },
+    {
+      "hash": "sha256-4StEWGrvA12mJBYZv6OGLaTaRPl7TQ2ahSX9uEkJBig=",
+      "url": "_framework/System.Text.Encodings.Web.wasm"
+    },
+    {
+      "hash": "sha256-AJnZjWotYDt30QhH01sU1jr7Wpz0ieH0XHXacEU/Irk=",
+      "url": "_framework/System.Text.Json.wasm"
+    },
+    {
+      "hash": "sha256-lK8igXz6npoSb7Io1fWfKQ6WRHx+mJ4AGIL9l8Mh5W8=",
+      "url": "_framework/System.Text.RegularExpressions.wasm"
+    },
+    {
+      "hash": "sha256-6JGPsbWhxPIcQh+83RW32iCCbEhqyXYeaBaXyb/El+c=",
+      "url": "_framework/blazor.boot.json"
+    },
+    {
+      "hash": "sha256-gvHfnndfEu1tRf0rFb5988rWq7ITIotOaE8+AMbKYbc=",
+      "url": "_framework/blazor.webassembly.js"
+    },
+    {
+      "hash": "sha256-DPPzjPfJpPr/VjPhO9zZsm8iq3spFxBbc/u8HnQ36gU=",
+      "url": "_framework/dotnet.js"
+    },
+    {
+      "hash": "sha256-9NoXeVUo4WNrEqrjAp/y3Yc8NmDcymQkzMGOIbIpGio=",
+      "url": "_framework/dotnet.native.js"
+    },
+    {
+      "hash": "sha256-f6b8OMzPxZicfBuJQC7pY0XrqnffvHX76rqLeGpL7oI=",
+      "url": "_framework/dotnet.native.wasm"
+    },
+    {
+      "hash": "sha256-qzVt86q0GKgDNek5+WFBz2hU8jGxI86YJeZmNGXj/qM=",
+      "url": "_framework/dotnet.runtime.js"
+    },
+    {
+      "hash": "sha256-SZLtQnRc0JkwqHab0VUVP7T3uBPSeYzxzDnpxPpUnHk=",
+      "url": "_framework/icudt_CJK.dat"
+    },
+    {
+      "hash": "sha256-8fItetYY8kQ0ww6oxwTLiT3oXlBwHKumbeP2pRF4yTc=",
+      "url": "_framework/icudt_EFIGS.dat"
+    },
+    {
+      "hash": "sha256-L7sV7NEYP37/Qr2FPCePo5cJqRgTXRwGHuwF5Q+0Nfs=",
+      "url": "_framework/icudt_no_CJK.dat"
+    },
+    {
+      "hash": "sha256-8LTSrmUHiaqHDH/8/xxyajqRoJxNpbxBcdbxcRB+t60=",
+      "url": "css/app.css"
+    },
+    {
+      "hash": "sha256-klohr1fZKuWsdZ+Xw5a5xmOzSJirykLXydA/MEAbUtg=",
+      "url": "css/legacy.css"
+    },
+    {
+      "hash": "sha256-Ll14mKHp4TwEiBQVEkVpvVhB9vkUQn+kp7AZDYSmGV4=",
+      "url": "data/v2/badges.json"
+    },
+    {
+      "hash": "sha256-tFPD4WJahtr+JTMvznDZVlSGHKNxZED6btsBSndyY3M=",
+      "url": "data/v2/competitions.json"
+    },
+    {
+      "hash": "sha256-dRuyUnGSe3TtprGZBWQeTv7gRQ+kHaTlJsL7AVIF8h0=",
+      "url": "data/v2/seasons.json"
+    },
+    {
+      "hash": "sha256-oyhiyp+1Dhrgl5INUp+sAlQC5eJa4aa6iWyWJOMXmSw=",
+      "url": "data/v2/teams.json"
+    },
+    {
+      "hash": "sha256-DbpQaq68ZSb5IoPosBErM1QWBfsbTxpJqhU0REi6wP4=",
+      "url": "icon-192.png"
+    },
+    {
+      "hash": "sha256-oEo6d+KqX5fjxTiZk/w9NB3Mi0+ycS5yLwCKwr4IkbA=",
+      "url": "icon-512.png"
+    },
+    {
+      "hash": "sha256-VRyxYbpJeELLKvPqSHXwMvomSakYWRdCG6b+CF2yUqs=",
+      "url": "index.html"
+    },
+    {
+      "hash": "sha256-U5AjwJYMt/oiKu9XZq1rEa/8xui5l03qiolfzQyaMU0=",
+      "url": "manifest.webmanifest"
+    },
+    {
+      "hash": "sha256-HC2Gvt5Uty5/B2lfMe4RBrh4HrjTu9XcWgyY8ift7JU=",
+      "url": "photos/barone-nero.jpg"
+    },
+    {
+      "hash": "sha256-g//FDnnCvjygmK2FnLB6+h/JlWKwe5T1HlkGFYkGL8g=",
+      "url": "photos/castillo.jpg"
+    },
+    {
+      "hash": "sha256-SPxh3mZ/BH5tF2aD+sqj2m+sMGMLr5Z74LQPnssakrk=",
+      "url": "photos/donald-j-trump.jpg"
+    },
+    {
+      "hash": "sha256-c3WVgupOl/oMYNdSBLUgpRUJB2K3Cy8wHN+408gdGE0=",
+      "url": "photos/hassan-black.jpg"
+    },
+    {
+      "hash": "sha256-o9ZyxUGWIwFGbX10ZAMAGUwPZ6sWRMbubUMZCnzS7bQ=",
+      "url": "photos/johnny-greenteeth.png"
+    },
+    {
+      "hash": "sha256-NplxGirpYqYVjv8YzPsCum8V8NwSolaW+6bqeIcUnhY=",
+      "url": "photos/peepoli.JPG"
+    },
+    {
+      "hash": "sha256-ROX3TCKEw4+/SF0SABQHAWQNQCDWbFEyMys3deO0ATQ=",
+      "url": "standings/2021-22_campionato_apertura.PNG"
+    },
+    {
+      "hash": "sha256-sJdoxjs4c7wy0ekNiUrzCKZOFV0Qwk6P1t4+imCoQec=",
+      "url": "standings/2021-22_campionato_clausura.PNG"
+    },
+    {
+      "hash": "sha256-dgK76+Xo+thaIWLWDSb+9eudKdsXbhbmUlakjIz7sW0=",
+      "url": "standings/2022-23_campionato_apertura.PNG"
+    },
+    {
+      "hash": "sha256-9WbEj61AOqEREFTC2W9mrj8RAXiazW75Or7uNTF6Lt4=",
+      "url": "standings/2023-24_campionato.PNG"
+    },
+    {
+      "hash": "sha256-ftY26euBVjunLenGs2kdsd091+CXnx8UylKoG2j4mTs=",
+      "url": "standings/2024-25_campionato.png"
+    },
+    {
+      "hash": "sha256-wSkav7GAghUrjKqCLUioemmw0DOWMGQnVxZtGzUqoj0=",
+      "url": "standings/2024-25_latrina.png"
+    },
+    {
+      "hash": "sha256-x0a7VfOJ16d/hBaGoVpgBBbOWki5rfIo0Gy8C91GMOo=",
+      "url": "standings/2024-25_memorial.JPG"
+    },
+    {
+      "hash": "sha256-F6ldt2apELQcTz4bI5ZopEocozLqmCNH/XegMP/kF8U=",
+      "url": "standings/README.md"
+    },
+    {
+      "hash": "sha256-15AYDkJralBW++pP3q0hD9LHm3TtMsA90m9n5guOLQU=",
+      "url": "svg/100.svg"
+    },
+    {
+      "hash": "sha256-sOlzHgfMBS9WW3mT3kJDxGfq7YQHiYARd5rrMZzMhGg=",
+      "url": "svg/al-drow.svg"
+    },
+    {
+      "hash": "sha256-X95W2Up3/Unbh97SX7VaAgsHOjFDvxJq5F7eWet3OJA=",
+      "url": "svg/apertura.svg"
+    },
+    {
+      "hash": "sha256-94Hki/X/aEPFo7rCii5aoJSQdWDpdhZ/Wxyq9EuJ+VE=",
+      "url": "svg/apertura2.svg"
+    },
+    {
+      "hash": "sha256-EBMVoYq1tbw6QAa96LIi0QCmEaQeuqw0pTKXPY4NsD4=",
+      "url": "svg/apertura3.svg"
+    },
+    {
+      "hash": "sha256-vVBBduJ1V3rePWJGqoyw4e46sY9uTxrudfwo3StDi0U=",
+      "url": "svg/brickmingam.svg"
+    },
+    {
+      "hash": "sha256-mQ0pC64VKleW3YG6SziGs16NsWMGtNiK1zFcdfBWv44=",
+      "url": "svg/camicia.svg"
+    },
+    {
+      "hash": "sha256-n2kAIOS7qxPFRt7L1Fl5mxXIbAXZHEZADnWiflG9b44=",
+      "url": "svg/cento.svg"
+    },
+    {
+      "hash": "sha256-Y2X+REsR/VS0AmD78z47TEbTpI+TGnSwt7ZPvG03DIM=",
+      "url": "svg/clausura.svg"
+    },
+    {
+      "hash": "sha256-R6XTt19ERLnH0Z5UkRzxJNwEV7tWe3F267CaKyFKi4w=",
+      "url": "svg/clausura2.svg"
+    },
+    {
+      "hash": "sha256-DwCC34v58dLnkppBDRTcYxk5vzBTCDShbKzgHQ53+i0=",
+      "url": "svg/clausura3.svg"
+    },
+    {
+      "hash": "sha256-tYtYb9Q4nZowRHK90YOZaOaSH/9039A7jGfDuUikPAc=",
+      "url": "svg/cringester_united.svg"
+    },
+    {
+      "hash": "sha256-BrfZgleixfLwMEP6xcwYNAmzgAVaAofzETFJNA6APGA=",
+      "url": "svg/dinamo_kabul.svg"
+    },
+    {
+      "hash": "sha256-c0sxReyX9TVp6w0m0V6qHTreIYuIDiATzKYC5rOLhFE=",
+      "url": "svg/dinamo_trast.svg"
+    },
+    {
+      "hash": "sha256-NeB9KIlDoNDSmt2oQq3EnRTAgdMWzXW5x5aNXFmH1CE=",
+      "url": "svg/fc_astana.svg"
+    },
+    {
+      "hash": "sha256-GYDmUTCQG0HYoo5JlM/L+H5vwT9O9wYZywt82OzfFS0=",
+      "url": "svg/fernanasport.svg"
+    },
+    {
+      "hash": "sha256-Oek7IkCfTpUvo06FhJvA0G8aZWC6D8Dso8cfKMf5w24=",
+      "url": "svg/finalista_latrina.svg"
+    },
+    {
+      "hash": "sha256-Jxaz9NerehUj6CTxtV5Fz3QcVEUuAyklt62TP/4sbu0=",
+      "url": "svg/finalista_memorial.svg"
+    },
+    {
+      "hash": "sha256-afvejtWS/bSuV2g09uwv47cIYrW//LamQ/0OEVIEKuc=",
+      "url": "svg/finalistasupercoppamoncada.svg"
+    },
+    {
+      "hash": "sha256-JlLp85TIMICSbK/MiR3rK+o2tLiZnjpMQGtfkxYBfjo=",
+      "url": "svg/g43.svg"
+    },
+    {
+      "hash": "sha256-vS8Occ1tt5mIvc55oEexzcsDXg4v1TY3yxLSEmlmBZE=",
+      "url": "svg/hapoel_rainbows.svg"
+    },
+    {
+      "hash": "sha256-M25xRgTPQJdSN7Qm0Et/cvdzxN9IuzVSmR7Xs/jPkx4=",
+      "url": "svg/kimbocats.svg"
+    },
+    {
+      "hash": "sha256-ej95aWEZaZm83U3yssGE5N728qazvYEnrimrBPLbbgU=",
+      "url": "svg/latrina.svg"
+    },
+    {
+      "hash": "sha256-f/Q2dwczVhl1zYxJfAfV6ztw4NHwc/Dpn1KBy5Hn958=",
+      "url": "svg/magisterauschwitz.svg"
+    },
+    {
+      "hash": "sha256-D8PQw0EQbeQymvjZz/IODmqoNpKqNXOnpnZdbBZxr1k=",
+      "url": "svg/maglia_nera.svg"
+    },
+    {
+      "hash": "sha256-iUsAbGQJh0jM8Zsgjyheh4skEoppGFsx6IfBkhQ5SVE=",
+      "url": "svg/memorial.svg"
+    },
+    {
+      "hash": "sha256-nr6qax4J3TMMxF65aFVxTr6QyK8FsUqsixGwtIKUTLc=",
+      "url": "svg/moncada.svg"
+    },
+    {
+      "hash": "sha256-OLyj/RGUFQ+JWn0roa5+1k1n92omFenkoVe3OXFMYes=",
+      "url": "svg/moncada_oro.svg"
+    },
+    {
+      "hash": "sha256-AYWMpWtRZrBPlu7VyxK22wQFLrLTIg/W8sbWNcqWvwo=",
+      "url": "svg/og.svg"
+    },
+    {
+      "hash": "sha256-vIa0kEP7b85cltXCjXQhRz2/w6S65R0YoI/oeJnWAME=",
+      "url": "svg/paccianese.svg"
+    },
+    {
+      "hash": "sha256-3B5Y5j/g7GKj0v1+AILZzjQe/uc8IqGLqS9O3JoCMZs=",
+      "url": "svg/piaseinsa.svg"
+    },
+    {
+      "hash": "sha256-DyuWD7rlNYIm1Y0uJnxtKhI9d6benSDYvOlZcchNIsg=",
+      "url": "svg/pisneloteam.svg"
+    },
+    {
+      "hash": "sha256-HOCgPHDhweB4ALIKnST0F5Gk4FPEfsVn4n0xC4zUIkg=",
+      "url": "svg/sborra_charrua.svg"
+    },
+    {
+      "hash": "sha256-HaywW/1SouQnldAAkjglad63+el/NqYKgVK0cBG1/cQ=",
+      "url": "svg/schalke_88.svg"
+    },
+    {
+      "hash": "sha256-759vPL65h/Ix9y8l3+jCSpZ1ai2hqMHus0fTXB5ZBnQ=",
+      "url": "svg/scialche_104.svg"
+    },
+    {
+      "hash": "sha256-n5GGdw+dSbCKG6aS1QnQ577XOF8e/FylkFit8yRuXag=",
+      "url": "svg/scudetto.svg"
+    },
+    {
+      "hash": "sha256-nCPOeIjAeUZ9IS6hDbKWYY15DOQ9RjeeXF1N0wKPZMA=",
+      "url": "svg/scudetto2.svg"
+    },
+    {
+      "hash": "sha256-A/1+lcKagXsbIj1kISN3ym3X/IpaEU+G/nZC51LVBvE=",
+      "url": "svg/scudetto3.svg"
+    },
+    {
+      "hash": "sha256-7J7GUp2PzbSzjuBfiAn52Rs3dC76KOi0zC2wkdumzxg=",
+      "url": "svg/ss_legio_sizziana.svg"
+    },
+    {
+      "hash": "sha256-7iaef7GdT731dYhbOCj8CisLjS9RALF0cVZ7nKAZBaE=",
+      "url": "svg/ss_ungern_khancheri.svg"
+    },
+    {
+      "hash": "sha256-qDKP9CMCjmIBWTzEcahhDjL2KH/XOWl2d59suuLxe30=",
+      "url": "svg/supercoppamoncada.svg"
+    },
+    {
+      "hash": "sha256-07pqIbCVriCZw5kmZZE/r0VGC/85RYFkSPB97rKFlBw=",
+      "url": "svg/team_chili.svg"
+    },
+    {
+      "hash": "sha256-b/oYLXJaX+6wvCMwa4ep+1SK5mZL6IVM3rSmraR6lqU=",
+      "url": "svg/uganda.svg"
+    },
+    {
+      "hash": "sha256-1/1eUk8YJeH5NIQwth3Ljis4xGfoAMFjOUxjvZ07pbs=",
+      "url": "svg/uganda_brics.svg"
+    },
+    {
+      "hash": "sha256-Lq2YmaSNEaX+VVJwb1VQST5deH9xAhuVvvuskn6LMa0=",
+      "url": "svg/zyklon_boys.svg"
+    }
+  ]
+};
---
+++ wwwroot/standings/README.md
@@ -0,0 +1,38 @@
+# Cartella Classifiche
+
+Questa cartella contiene le immagini delle classifiche finali delle competizioni del Fantamattone.
+
+## Convenzione di Nomenclatura
+
+I file devono seguire il formato: `{STAGIONE}_{TIPO_COMPETIZIONE}.{ESTENSIONE}`
+
+### Formato Stagione
+- Utilizzare trattini invece di barre: `2021-22` (non `2021/22`)
+- Esempi: `2021-22`, `2022-23`, `2023-24`
+
+### Tipi di Competizione (tutto minuscolo)
+- `campionato` - Campionato principale
+- `apertura` - Torneo di Apertura
+- `clausura` - Torneo di Clausura  
+- `memorial` - Memorial
+- `latrina` - Coppa Latrina
+
+### Estensioni Supportate
+- `.png` (consigliato per qualità)
+- `.jpg` / `.jpeg`
+- `.webp` (formato moderno, dimensioni ridotte)
+
+## Esempi di Nomi File Validi
+
+- `2021-22_campionato.png`
+- `2022-23_apertura.jpg`
+- `2023-24_clausura.png`
+- `2021-22_memorial.jpeg`
+- `2022-23_latrina.webp`
+- `2024-25_campionato.webp`
+
+## Note
+
+- I nomi dei file sono case-sensitive
+- Assicurarsi che non ci siano spazi nei nomi dei file
+- Le immagini verranno caricate automaticamente nella pagina delle competizioni corrispondenti 
